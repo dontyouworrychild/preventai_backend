eecf08c52158b26bdbc2d3badba369c7bad176b3
saiyngali <myrzasaiyngali@gmail.com>
2023-11-27 18:10:43 +0600

    Update url

--- api/patient/urls.py
+++ api/patient/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RehabCenterViewSet, PatientViewSet, InterviewViewSet
+from .views import *
 
 router = DefaultRouter()
 router.register(r'rehab_centers', RehabCenterViewSet)
 router.register(r'patients', PatientViewSet)
 router.register(r'interviews', InterviewViewSet)
+router.register(r'time_slots', TimeSlotViewSet)
+router.register(r'doctrs', DoctorViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
